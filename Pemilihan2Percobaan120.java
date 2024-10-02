ef6b67643145949e676757019f54e8dd202ceb6f
shafiqa-nabila <shafiqanabila9@gmail.com>
2024-10-02 09:34:41 -1200

    tambahkan pemilihan 2

---
+++ Pemilihan2Percobaan120.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Pemilihan2Percobaan120
+ */
+public class Pemilihan2Percobaan120 {
+
+    public static void main(String[] args) {
+         Scanner input20 = new Scanner(System.in);
+         int tahun = input20.nextInt();
+
+         if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0);
+                System.out.println("Tahun Kabisat");
+         } else 
+            System.out.println("Bukan Tahun Kabisat");
+    }
+}
